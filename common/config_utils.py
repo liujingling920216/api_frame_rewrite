7cc39c4cb5fb6274106fabcf2c5d86ebd0a68de4
sophia <1040616989@qq.com>
2020-08-04 23:30:58 +0800

    接口测试框架第二次提交： 封装log_utils

--- common/config_utils.py
+++ common/config_utils.py
@@ -1,7 +1,7 @@
 import os
 import configparser
 
-conf_file = os.path.join(os.path.dirname(__file__),'..\conf\config.ini')
+conf_file = os.path.join(os.path.dirname(__file__),'../conf/config.ini')
 
 class Config:
     def __init__(self):
@@ -10,12 +10,23 @@ class Config:
 
     @property
     def excel_path(self):
-        excel_path = self.config.get('path', 'excel_path')
+        excel_path = self.config.get('PATH', 'excel_path')
         return excel_path
+
+    @property
+    def logs_path(self):
+        log_path = self.config.get('PATH', 'log_path')
+        return log_path
+
+    @property
+    def log_lever(self):
+        log_lever = int(self.config.get('LOG','log_lever'))
+        return log_lever
 
 config = Config()
 
 if __name__ == '__main__':
     print(config.excel_path)
+    print(config.logs_path)
 
 
---
+++ common/log_utils.py
@@ -0,0 +1,35 @@
+import os
+import time
+import logging
+from common.config_utils import config
+
+log_output_path = os.path.join(os.path.dirname(__file__),config.logs_path)
+
+class Log:
+    def __init__(self):
+        self.log_name = os.path.join(log_output_path,'api_test_%s.log'%(time.strftime('%Y_%m_%d')))
+        self.logger = logging.getLogger('test.log')
+        self.logger.setLevel(config.log_lever)
+        self.format = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+
+
+    def streamlog(self):
+        stream_log = logging.StreamHandler()
+        stream_log.setFormatter(self.format)
+        self.logger.addHandler(stream_log)
+        stream_log.close()
+        return self.logger
+
+
+    def filelog(self):
+        file_log = logging.FileHandler(self.log_name,'a',encoding='utf-8')
+        file_log.setFormatter(self.format)
+        self.logger.addHandler(file_log)
+        file_log.close()
+        return self.logger
+
+log = Log().filelog()
+
+if __name__ == "__main__":
+    # log.info('ceshi ')
+    log.info('ceshi ')
